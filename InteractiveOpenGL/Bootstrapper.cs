809f5430788ddb72d5cc5881f200379761a73077
Piotr Szymczyk <ja.piotrek.szymczyk@gmail.com>
2017-04-10 02:58:52 +0200

    Initial commit

---
+++ InteractiveOpenGL/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mef;
+using System.Windows;
+using InteractiveOpenGL.Views;
+
+namespace InteractiveOpenGL
+{
+	class Bootstrapper : MefBootstrapper
+	{
+		protected override DependencyObject CreateShell()
+		{
+			return new Shell();
+		}
+
+		protected override void InitializeShell()
+		{
+			Application.Current.MainWindow.Show();
+		}
+	}
+}
---
+++ InteractiveOpenGL/Views/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace InteractiveOpenGL.Views
+{
+	/// <summary>
+	/// Logika interakcji dla klasy Shell.xaml
+	/// </summary>
+	public partial class Shell : Window
+	{
+		public Shell()
+		{
+			InitializeComponent();
+		}
+	}
+}
